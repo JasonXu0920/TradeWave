16d244189b60a0b0a905e66b2e0df313f98562c6
Jason Xu <97411709+JasonXu0920@users.noreply.github.com>
2024-04-01 16:38:26 +1100

    server side set up

---
+++ TradingEngineServer/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using TradingEngineServer.Core;
