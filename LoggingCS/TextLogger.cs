41735c608c7da9cbcc82b2f3f78ab3262b3fca30
Jason Xu <97411709+JasonXu0920@users.noreply.github.com>
2024-04-09 20:08:55 +1000

    TextLogger Configuration values

--- LoggingCS/TextLogger.cs
+++ LoggingCS/TextLogger.cs
@@ -2,17 +2,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using TradingEngineServer.Logging.LoggingConfiguration;
 
 namespace TradingEngineServer.Logging
 {
     public class TextLogger : AbstractLogger, ITextLogger
     {
-        public void Dispose()
+        private readonly LoggerConfiguration _loggingConfiguration;
+        public TextLogger(IOptions<LoggerConfiguration> loggingConfiguration) : base()
+        {
+            _loggingConfiguration = loggingConfiguration.Value ?? throw new ArgumentException(nameof(loggingConfiguration));
+        }
+
+        protected override void Log(LogLevel logLevel, string module, string message)
         {
             throw new NotImplementedException();
         }
 
-        protected override void Log(LogLevel logLevel, string module, string message)
+        public void Dispose()
         {
             throw new NotImplementedException();
         }
